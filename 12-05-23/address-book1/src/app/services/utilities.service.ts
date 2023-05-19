2d6a335d332f92c545501b32423c887a8cc7d8ed
Anastasia Tyurikova <anastasiatyrikova@gmail.com>
2023-05-19 11:53:23 +0200

    Feat: utility isNotEmptyArray added

--- 12-05-23/address-book1/src/app/services/utilities.service.ts
+++ 12-05-23/address-book1/src/app/services/utilities.service.ts
@@ -8,15 +8,20 @@ export class UtilitiesService {
 
   constructor() { }
 
+  /* metodo per convertite la stringa in title case */
   public toTitleCaseFunction (value: string): string {
-    console.log("ehii toTitleCaseFunction funziona!");
     return value.toLowerCase().replace(/^(.)|\s+(.)/g, ($1) => $1.toUpperCase());
   }
 
+  /* metodo che prende il formato di data dall'ipotetico database e lo converte in un altro format*/
   public dateFormatFunction (dateStr: string): string {
-    const now = moment(); // Создание объекта Moment, представляющего текущую дату и время
-    const formattedDate = moment(dateStr, 'YYYY-MM-DD').format('DD/MM/YYYY'); 
-    return formattedDate; // Вывод отформатированной даты в консоль
-
+    const formattedDate = moment(dateStr, 'YYYY-MM-DD').format('DD/MM/YYYY');
+    return formattedDate;
   }
+
+  /* metodo che controlla se entrambi parametri sono true: array esiste e la sua lunghezza e maggiore a 0 */
+  public isNotEmptyArray(arr: any[]): boolean {
+    return Array.isArray(arr) && arr.length > 0;
+  }
+
 }
