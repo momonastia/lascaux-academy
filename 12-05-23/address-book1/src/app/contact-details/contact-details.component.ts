52c69b619674dbe8624f2691dd690fdd37182666
Anastasia Tyurikova <anastasiatyrikova@gmail.com>
2023-05-19 10:47:45 +0200

    Feat: services utilities added

--- 12-05-23/address-book1/src/app/contact-details/contact-details.component.ts
+++ 12-05-23/address-book1/src/app/contact-details/contact-details.component.ts
@@ -1,12 +1,14 @@
-import { Component, EventEmitter, Output, Input } from '@angular/core';
+import { Component, EventEmitter, Output, Input, OnInit } from '@angular/core';
 import { Contact } from '../models/contact.module';
+import { UtilitiesService } from '../services/utilities.service';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-contact-details',
   templateUrl: './contact-details.component.html',
   styleUrls: ['./contact-details.component.css']
 })
-export class ContactDetailsComponent {
+export class ContactDetailsComponent implements OnInit  {
 
   @Input('selectedContactForDetails') selectedContact: Contact | null | undefined;
 
@@ -14,6 +16,28 @@ export class ContactDetailsComponent {
 
   backToContactList(){
     this.backToContactListEvent.emit();
+  }
+
+  convertedText: string;
+  convertedDate: string;
+
+  constructor(private utilitiesService: UtilitiesService) {}
+
+  ngOnInit() {
+    this.convertText();
+    this.convertBirthDate()
+  }
+
+  convertText(){
+    if (this.selectedContact) {
+      this.convertedText = this.utilitiesService.toTitleCaseFunction(this.selectedContact.firstName);
+    }
+  }
+
+  convertBirthDate() {
+    if (this.selectedContact) {
+      this.convertedDate = this.utilitiesService.dateFormatFunction(this.selectedContact.birthDate);
+    }
   }
 
 }
--- 12-05-23/address-book1/src/app/models/contact.module.ts
+++ 12-05-23/address-book1/src/app/models/contact.module.ts
@@ -4,4 +4,5 @@ export interface Contact {
   phoneNumber: string;
   phonePrefix: string;
   email?: string;
+  birthDate: string;
 }
