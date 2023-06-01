df3de087adf958d5db5594bf0bacbfe676871867
Anastasia Tyurikova <anastasiatyrikova@gmail.com>
2023-06-01 18:25:55 +0200

    Feat: reactive form validations added

--- 12-05-23/address-book1/src/app/add-contact-react/add-contact-react.component.ts
+++ 12-05-23/address-book1/src/app/add-contact-react/add-contact-react.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
 import { ContactDetails } from '../models/contact.module';
+import { combineLatest, filter, merge } from 'rxjs';
 
 @Component({
   selector: 'app-add-contact-react',
@@ -19,13 +20,29 @@ export class AddContactReactComponent implements OnInit {
     this.contactForm = this.formBuilder.group({
      firstName: ["Nastia", Validators.required],
      lastName: ["", Validators.required],
-     phoneNumber: ["", Validators.required],
-     phonePrefix: ["", Validators.required],
-     gender: ["M", Validators.required],
-     email: ["", [Validators.required, Validators.email]],
-     birthDate: ["1996-07-12", Validators.required],
-     drivingLicence: [false, Validators.requiredTrue],
+     details: this.formBuilder.group({
+
+        phoneNumber: ["", Validators.required],
+        phonePrefix: ["", Validators.required],
+        gender: ["M", Validators.required],
+        email: ["", [Validators.required, Validators.email]],
+        birthDate: ["1996-07-12", Validators.required],
+        drivingLicence: [false, Validators.requiredTrue],
+
     })
+    })
+
+    this.contactForm.get("details")!.disable()
+
+    /* this.contactForm.get("firstName")?.valueChanges.subscribe(console.log); */
+    combineLatest([
+      this.contactForm.get("firstName")!.statusChanges,
+      this.contactForm.get("lastName")!.statusChanges
+    ]).pipe(filter(
+      (s) => s.every((status) => status === "VALID")))
+    .subscribe(() =>
+      this.contactForm.get("details")!.enable()
+    );
   }
 
 
