842dfc8238a17aafa9ebcf4caea8bc33722f24f4
Anastasia Tyurikova <anastasiatyrikova@gmail.com>
2023-06-05 18:50:51 +0200

    lezione 05-06-23

--- 12-05-23/address-book1/src/app/add-contact-react/add-contact-react.component.ts
+++ 12-05-23/address-book1/src/app/add-contact-react/add-contact-react.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
-import { ContactDetails } from '../models/contact.module';
-import { combineLatest, filter, merge } from 'rxjs';
+import { FormArray, FormBuilder, FormControlStatus, FormGroup, NgForm, ValidatorFn, Validators } from '@angular/forms';
+import { ContactDetails, ContactForm, Education, LanguageLevel, LanguageSkill } from '../models/contact.module';
+import { combineLatest, filter, interval, map, merge, take, tap } from 'rxjs';
+import { ValidationErrors } from '@angular/forms';
+import { AsyncValidatorFn } from '@angular/forms';
 
 @Component({
   selector: 'app-add-contact-react',
@@ -11,6 +13,28 @@ import { combineLatest, filter, merge } from 'rxjs';
 export class AddContactReactComponent implements OnInit {
 
   contactForm!: FormGroup
+  contact: ContactDetails = {
+    firstName: "Ana",
+    lastName: "Tyurikova",
+    birthDate: "27.09.1986",
+    drivingLicence: true,
+    education: Education.degree,
+    email: "anastasiatyrikova@gmail.com",
+    gender: "F",
+    phoneNumber: "3922989762",
+    phonePrefix: "+39",
+    id: 1,
+  }
+  languageSkills: LanguageSkill[] = [
+    {
+      language: "Inglese",
+      level: LanguageLevel.A1,
+    },
+    {
+      language: "Spagnolo",
+      level: LanguageLevel.A1,
+    },
+  ]
 
   constructor(
     private formBuilder: FormBuilder
@@ -18,10 +42,20 @@ export class AddContactReactComponent implements OnInit {
 
   ngOnInit(): void {
     this.contactForm = this.formBuilder.group({
-     firstName: ["Nastia", Validators.required],
-     lastName: ["", Validators.required],
+     firstName: ["", [Validators.required, this.upperCaseValidator()]],
+     lastName: ["", Validators.required, this.upperCaseValidatorAsync()],
      details: this.formBuilder.group({
-
+        /* languageSkills: this.formBuilder.array([
+          this.formBuilder.group({
+          language: "Inglese",
+          level: LanguageLevel.A1,
+        }
+        )]), */
+        languageSkills: this.formBuilder.array(
+          this.languageSkills.map(langskills =>
+            this.formBuilder.group(langskills)
+            )
+        ),
         phoneNumber: ["", Validators.required],
         phonePrefix: ["", Validators.required],
         gender: ["M", Validators.required],
@@ -32,21 +66,108 @@ export class AddContactReactComponent implements OnInit {
     })
     })
 
-    this.contactForm.get("details")!.disable()
+   /*  this.contactForm.get("details")!.disable() */
 
     /* this.contactForm.get("firstName")?.valueChanges.subscribe(console.log); */
+
+    this.contactForm.patchValue(this.contact)
+    this.contactForm.get("details")?.patchValue(this.contact)
+
     combineLatest([
       this.contactForm.get("firstName")!.statusChanges,
       this.contactForm.get("lastName")!.statusChanges
-    ]).pipe(filter(
-      (s) => s.every((status) => status === "VALID")))
+    ]).pipe(
+      tap(console.log),
+      filter(
+      (s) => s.every((status: FormControlStatus) => status === "VALID")))
     .subscribe(() =>
       this.contactForm.get("details")!.enable()
     );
   }
 
+  upperCaseValidator(): ValidatorFn {
 
-  saveForm(form: ContactDetails): void {
-    console.log("ContactDetails", form)
+    return (control) => {
+      console.log("Validatore custom", control.value);
+      control.errors
+
+      const upperCaseError: ValidationErrors = {
+        upperCase : true
+      }
+
+      if(control.value == control.value.toUpperCase()){
+        return null
+      } else {
+        return upperCaseError
+      }
+    }
+  }
+
+  upperCaseValidatorAsync(): AsyncValidatorFn {
+
+    return (control) => {
+      console.log("Validatore custom", control.value);
+      return interval(15000).pipe(
+        take(1),
+        map(_ => {
+          control.errors;
+          const upperCaseError: ValidationErrors = {
+            asyncUpperCase : true
+          }
+
+          if(control.value == control.value.toUpperCase()){
+            return null
+          } else {
+            return upperCaseError
+          }
+        })
+      )
+
+
+    }
+  }
+
+  saveForm(form: ContactForm): void {
+    console.log("Contact Details", form)
+
+    const {details, ...others} = form
+    const newContact: ContactDetails = {
+      /* firstName: form.firstName,
+      lastName: form.lastName,
+      birthDate: form.details.birthDate,
+      drivingLicence: form.details.drivingLicence,
+      education: form.details.education,
+      email: form.details.email,
+      gender: form.details.gender,
+      id: 1,
+      phoneNumber: form.details.phoneNumber,
+      phonePrefix: form.details.phonePrefix,
+      languageSkills: form.details.languageSkills, */
+
+      ...others,
+      ...details,
+      id: 1,
+    }
+      console.log("newContact", newContact)
+  }
+
+  addLanguage(): void {
+    const newLanguageGroup: FormGroup = this.formBuilder.group({
+      language: ["", Validators.required],
+      level: LanguageLevel.A1,
+    });
+    (this.contactForm.get("details")?.get("languageSkills") as FormArray).push(newLanguageGroup)
+  }
+
+  get languageSkill(): FormArray {
+    return this.contactForm.get("details")?.get("languageSkills") as FormArray
+  }
+
+  removeLanguage(i: number): void {
+    this.languageSkill.removeAt(i)
+  }
+
+  removeAllLanguages(): void {
+    this.languageSkill.clear()
   }
 }
--- 12-05-23/address-book1/src/app/models/contact.module.ts
+++ 12-05-23/address-book1/src/app/models/contact.module.ts
@@ -4,6 +4,19 @@ export interface Contact {
   lastName: string;
 }
 
+export interface LanguageSkill {
+  language: string;
+  level: LanguageLevel
+}
+
+export enum LanguageLevel {
+  A1,
+  A2,
+  B1,
+  B2,
+  C1,
+  C2
+ }
 export interface ContactDetails extends Contact {
   gender: "M" | "F" | "N";
   phoneNumber: string;
@@ -12,10 +25,16 @@ export interface ContactDetails extends Contact {
   birthDate: Date | string ;
   drivingLicence: boolean;
   education: Education;
+  languageSkills?: LanguageSkill[]
  }
 
  export enum Education {
   middleLicence,
   diploma,
   degree
+ }
+
+
+ export interface ContactForm extends Omit<Contact, 'id'> {
+  details: Omit<ContactDetails, 'id' | 'firstName' | 'lastName'>;
  }
