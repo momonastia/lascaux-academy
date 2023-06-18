c4849b933b707b6f4baad74104bef7cdb518290c
momonastia~ <anastasiatyrikova@gmail.com>
2023-06-18 16:05:50 +0200

    Esercitazione rxjs page about

--- 12-05-23/address-book1/src/app/pages/about-page/about-page.component.ts
+++ 12-05-23/address-book1/src/app/pages/about-page/about-page.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { from, merge } from 'rxjs';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-about-page',
   templateUrl: './about-page.component.html',
   styleUrls: ['./about-page.component.css']
 })
-export class AboutPageComponent {
+export class AboutPageComponent implements OnInit {
 
+  /* Объединение двух Observable */
+
+  source1 = from([1, 2, 3, 4, 5]);
+  source2 = from(['A', 'B', 'C', 'D', 'E']);
+  mergedValues: any[] = [];
+
+  merged = merge(this.source1,this.source2).subscribe
+    (value => {
+      console.log(value);
+      this.mergedValues.push(value)
+    });
+
+  /* Фильтрация */
+
+  numbers = from([1, 2, 3, 4, 5]);
+
+  ngOnInit() {
+    this.numbers.pipe(
+      filter(number => number % 2 === 0)
+    ).subscribe(value => console.log("Фильтрация четных чисел", value))
+
+  }
 }
+
+
+
+
+
+
+
+
+
